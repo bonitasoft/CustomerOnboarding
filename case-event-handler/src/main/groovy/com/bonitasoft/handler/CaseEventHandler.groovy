4c4c13011df121c4dcc4997766ed23f85d0da2f5
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-11-29 17:12:57 +0100

    refactor evant handler and rest api extensions

--- case-event-handler/src/main/groovy/com/bonitasoft/handler/CaseEventHandler.groovy
+++ case-event-handler/src/main/groovy/com/bonitasoft/handler/CaseEventHandler.groovy
@@ -1,45 +1,21 @@
 package com.bonitasoft.handler;
 
-import java.io.Serializable
-
 import org.bonitasoft.engine.api.ProcessAPI
-import org.bonitasoft.engine.api.impl.APIUtils
 import org.bonitasoft.engine.api.impl.ProcessAPIImpl
-import org.bonitasoft.engine.bpm.category.Category
-import org.bonitasoft.engine.bpm.category.CategoryCriterion
-import org.bonitasoft.engine.bpm.connector.ConnectorDefinition
 import org.bonitasoft.engine.bpm.data.DataDefinition
-import org.bonitasoft.engine.bpm.data.DataInstance
-import org.bonitasoft.engine.bpm.data.DataNotFoundException
-import org.bonitasoft.engine.bpm.data.impl.ShortTextDataInstanceImpl
 import org.bonitasoft.engine.bpm.flownode.ActivityDefinitionNotFoundException
 import org.bonitasoft.engine.bpm.flownode.ActivityInstanceSearchDescriptor
 import org.bonitasoft.engine.bpm.flownode.ActivityStates
 import org.bonitasoft.engine.bpm.flownode.HumanTaskInstance
-import org.bonitasoft.engine.bpm.flownode.UserTaskInstance
-import org.bonitasoft.engine.bpm.process.DesignProcessDefinition
-import org.bonitasoft.engine.bpm.process.ProcessDefinition
-import org.bonitasoft.engine.core.process.instance.api.states.FlowNodeState
 import org.bonitasoft.engine.core.process.instance.model.SUserTaskInstance
-import org.bonitasoft.engine.data.instance.api.DataInstanceContainer
-import org.bonitasoft.engine.data.instance.exception.SDataInstanceException
-import org.bonitasoft.engine.data.instance.model.SDataInstance
 import org.bonitasoft.engine.events.model.SEvent
 import org.bonitasoft.engine.events.model.SHandler
 import org.bonitasoft.engine.events.model.SHandlerExecutionException
 import org.bonitasoft.engine.events.model.SUpdateEvent
-import org.bonitasoft.engine.expression.ExpressionConstants
 import org.bonitasoft.engine.expression.ExpressionType
-import org.bonitasoft.engine.expression.model.SExpression
-import org.bonitasoft.engine.log.technical.TechnicalLogSeverity
-import org.bonitasoft.engine.log.technical.TechnicalLoggerService
-import org.bonitasoft.engine.persistence.OrderByType
-import org.bonitasoft.engine.recorder.model.EntityUpdateDescriptor
 import org.bonitasoft.engine.search.SearchOptionsBuilder
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-
-import javassist.bytecode.stackmap.BasicBlock.Catch
 
 
 class CaseEventHandler implements SHandler<SEvent> {
@@ -48,8 +24,12 @@ class CaseEventHandler implements SHandler<SEvent> {
 	private static final String STATE_UPDATED = "ACTIVITYINSTANCE_STATE_UPDATED"
 	private static final String ACTIVITY_CONTAINER = "Dymanic Activity Container"
 	private static final String CREATE_ACTIVITY = "Create Activity"
+	private String prefix
+	private List<String> ignoredActivities = []
 
-	public CaseEventHandler(){
+	public CaseEventHandler(String prefix, List<String> ignoredActivities){
+		this.prefix = prefix
+		this.ignoredActivities = ignoredActivities
 	}
 
 	@Override
@@ -57,41 +37,35 @@ class CaseEventHandler implements SHandler<SEvent> {
 		def SUserTaskInstance taskInstance =  event.getObject()
 		def processInstanceId = taskInstance.parentProcessInstanceId
 		ProcessAPI processAPI = new ProcessAPIImpl()
-		def caseActivities = processAPI.searchHumanTaskInstances(new SearchOptionsBuilder(0, Integer.MAX_VALUE).with {
-			filter(ActivityInstanceSearchDescriptor.PROCESS_INSTANCE_ID, processInstanceId)
-			and()
-			differentFrom(ActivityInstanceSearchDescriptor.NAME, ACTIVITY_CONTAINER)
-			and()
-			differentFrom(ActivityInstanceSearchDescriptor.NAME, CREATE_ACTIVITY)
-			and()
-			filter(ActivityInstanceSearchDescriptor.STATE_NAME, ActivityStates.READY_STATE)
-			done()
-		}).getResult()
+		def builder = new SearchOptionsBuilder(0, Integer.MAX_VALUE)
+				.filter(ActivityInstanceSearchDescriptor.PROCESS_INSTANCE_ID, processInstanceId)
+				.and()
+				.filter(ActivityInstanceSearchDescriptor.STATE_NAME, ActivityStates.READY_STATE)
+		ignoredActivities.each{ builder.and().differentFrom(ActivityInstanceSearchDescriptor.NAME, it)}
+		def caseActivities = processAPI.searchHumanTaskInstances(builder.done()).getResult()
 		def pDefId = processAPI.getProcessDefinitionIdFromProcessInstanceId(processInstanceId)
-		def DesignProcessDefinition design = processAPI.getDesignProcessDefinition(pDefId)
-		def dataToUpdate = []
 		caseActivities.each{ HumanTaskInstance task ->
 			try {
-				processAPI.getActivityDataDefinitions(task.processDefinitionId, task.name, 0, Integer.MAX_VALUE)
-						.each{ DataDefinition dataDef ->
-							if(dataDef.name == "activityState"
-							&&dataDef.defaultValueExpression != null
-							&& dataDef.defaultValueExpression.expressionType != ExpressionType.TYPE_CONSTANT.toString()
-							&& dataDef.defaultValueExpression.content != null
-							&& !dataDef.defaultValueExpression.content.isEmpty()
-							&& !dataDef.defaultValueExpression.content.equals("[]")) {
-								def dataInstance = processAPI.getActivityTransientDataInstance(dataDef.name, task.id)
-								def deps = [:]
-								def expressions = [:]
-								expressions.put(dataDef.defaultValueExpression, deps)
-								def result = processAPI.evaluateExpressionsOnActivityInstance(task.id, expressions)
-								def newValue = result.values()[0]
-								println "$dataDef.name value of $task.id has been updated to $newValue"
-								processAPI.updateActivityTransientDataInstance("activityState",task.id, newValue)
-							}
+			processAPI.getActivityDataDefinitions(task.processDefinitionId, task.name, 0, Integer.MAX_VALUE)
+					.each{ DataDefinition dataDef ->
+						if( dataDef.isTransientData()
+						&& dataDef.name.startsWith(prefix)
+						&& dataDef.defaultValueExpression != null
+						&& dataDef.defaultValueExpression.expressionType != ExpressionType.TYPE_CONSTANT.toString()
+						&& dataDef.defaultValueExpression.content != null
+						&& !dataDef.defaultValueExpression.content.isEmpty()) {
+							def dataInstance = processAPI.getActivityTransientDataInstance(dataDef.name, task.id)
+							def deps = [:]
+							def expressions = [:]
+							expressions.put(dataDef.defaultValueExpression, deps)
+							def result = processAPI.evaluateExpressionsOnActivityInstance(task.id, expressions)
+							def newValue = result.values()[0]
+							processAPI.updateActivityTransientDataInstance(dataDef.name,task.id, newValue)
+							LOGGER.debug("$dataDef.name value of $task.id has been updated with: $newValue")
 						}
+					}
 			}catch(ActivityDefinitionNotFoundException e) {
-				println "Cannot retrieve data for task $task.name"
+				//ignore
 			}
 		}
 	}
--- restAPIExtensions/caseActivityRestAPI/src/main/groovy/com/bonitasoft/rest/api/CaseActivity.groovy
+++ restAPIExtensions/caseActivityRestAPI/src/main/groovy/com/bonitasoft/rest/api/CaseActivity.groovy
@@ -3,12 +3,17 @@ package com.bonitasoft.rest.api;
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+import org.bonitasoft.engine.bpm.data.DataInstance
 import org.bonitasoft.engine.bpm.data.DataNotFoundException
 import org.bonitasoft.engine.bpm.flownode.ActivityInstance
+import org.bonitasoft.engine.bpm.flownode.ActivityInstanceCriterion
 import org.bonitasoft.engine.bpm.flownode.ActivityInstanceSearchDescriptor
 import org.bonitasoft.engine.bpm.flownode.ActivityStates
 import org.bonitasoft.engine.bpm.flownode.ArchivedActivityInstanceSearchDescriptor
+import org.bonitasoft.engine.bpm.flownode.ArchivedHumanTaskInstance
 import org.bonitasoft.engine.bpm.flownode.HumanTaskDefinition
+import org.bonitasoft.engine.bpm.flownode.HumanTaskInstance
+import org.bonitasoft.engine.bpm.flownode.HumanTaskInstanceSearchDescriptor
 import org.bonitasoft.engine.bpm.flownode.ManualTaskInstance
 import org.bonitasoft.engine.bpm.flownode.StandardLoopCharacteristics
 import org.bonitasoft.engine.bpm.flownode.UserTaskInstance
@@ -24,71 +29,104 @@ import com.bonitasoft.web.extension.rest.RestAPIContext
 import com.bonitasoft.web.extension.rest.RestApiController
 
 import groovy.json.JsonBuilder
+import groovy.sql.DataSet
 
 class CaseActivity implements RestApiController,CaseActivityHelper {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CaseActivity.class)
+	private static final Logger LOGGER = LoggerFactory.getLogger(CaseActivity.class)
 	private static final String ACTIVITY_CONTAINER = "Dymanic Activity Container"
 	private static final String CREATE_ACTIVITY = "Create Activity"
+	private static final String PREFIX = '$'
 
-    @Override
-    RestApiResponse doHandle(HttpServletRequest request, RestApiResponseBuilder responseBuilder, RestAPIContext context) {
+	@Override
+	RestApiResponse doHandle(HttpServletRequest request, RestApiResponseBuilder responseBuilder, RestAPIContext context) {
 		def contextPath = "http://$request.serverName:$request.localPort$request.contextPath"
-        def caseId = request.getParameter "caseId"
-        if (!caseId) {
-            return buildResponse(responseBuilder, HttpServletResponse.SC_BAD_REQUEST,"""{"error" : "the parameter caseId is missing"}""")
-        }
+		def caseId = request.getParameter "caseId"
+		if (!caseId) {
+			return buildResponse(responseBuilder, HttpServletResponse.SC_BAD_REQUEST,"""{"error" : "the parameter caseId is missing"}""")
+		}
 
 		def ProcessAPI processAPI = context.apiClient.getProcessAPI()
 		def pDef = -1;
 		try {
-			 pDef = processAPI.getProcessDefinition(processAPI.getProcessDefinitionIdFromProcessInstanceId(caseId.toLong()))
+			pDef = processAPI.getProcessDefinition(processAPI.getProcessDefinitionIdFromProcessInstanceId(caseId.toLong()))
 		}catch(ProcessDefinitionNotFoundException e) {
 			return buildResponse(responseBuilder, HttpServletResponse.SC_NOT_FOUND,"""{"error" : "no process definition found for instance $caseId" }""")
 		}
-	
+
 		def designProcessDefinition = processAPI.getDesignProcessDefinition(pDef.id)
 		def loopTasks = designProcessDefinition.getFlowElementContainer().getActivities().findAll{
 			it instanceof HumanTaskDefinition && it.getLoopCharacteristics() instanceof StandardLoopCharacteristics
-		}.collect{
-			it.name
-		}
-		def result = []
-		//Retrieve pending activities
-		searchOpenedTasks(caseId.toLong(), processAPI).getResult().collect{
-			def state = getState(it,processAPI)
-			if(canExecute(state)) {
-				result << [id:it.name,name:it.displayName,state:state,url:forge(pDef.name,pDef.version,it,contextPath),description:it.description,target:linkTarget(it)]
-			}else {
-				result << [id:it.name,name:it.displayName,state:state,description:it.description]
-			}
-		}
+		}.collect{ it.name }
+		def result = processAPI.getPendingHumanTaskInstances(context.apiSession.userId,0, Integer.MAX_VALUE, ActivityInstanceCriterion.EXPECTED_END_DATE_ASC)
+				.findAll{it.name != ACTIVITY_CONTAINER && it.name != CREATE_ACTIVITY }
+				.collect{ HumanTaskInstance task ->
+					[
+						id:task.name,
+						name:task.displayName ?: task.name,
+						url:forge(pDef.name,pDef.version,task,contextPath),
+						description:task.description,
+						target:linkTarget(task),
+						state:task.state.capitalize(),
+						metadata:getMetadata(task,processAPI)
+					]
+				}
+
+		def containerInstance = processAPI.searchHumanTaskInstances(new SearchOptionsBuilder(0, 1)
+				.filter(HumanTaskInstanceSearchDescriptor.PROCESS_INSTANCE_ID, caseId)
+				.filter(HumanTaskInstanceSearchDescriptor.NAME, ACTIVITY_CONTAINER)
+				.done()).result[0]
+
+		println "$containerInstance.name=$containerInstance.id"
+
+		//Retrieve adhoc activities
+		result.addAll(processAPI.searchHumanTaskInstances(new SearchOptionsBuilder(0, Integer.MAX_VALUE)
+				.filter(HumanTaskInstanceSearchDescriptor.PARENT_CONTAINER_ID, containerInstance.id)
+				.done())
+				.result
+				.collect{ HumanTaskInstance task ->
+					[
+						id:task.name,
+						name:task.displayName ?: task.name,
+						url:forge(pDef.name,pDef.version,task,contextPath),
+						description:task.description,
+						target:linkTarget(task),
+						state:task.state.capitalize(),
+						metadata:getMetadata(task,processAPI)
+					]
+				})
+
 		//Retrieve finished activities
-		processAPI.searchArchivedHumanTasks(new SearchOptionsBuilder(0, Integer.MAX_VALUE).with {
+		result.addAll(processAPI.searchArchivedHumanTasks(new SearchOptionsBuilder(0, Integer.MAX_VALUE).with {
 			filter(ArchivedActivityInstanceSearchDescriptor.PARENT_PROCESS_INSTANCE_ID, caseId)
 			differentFrom(ArchivedActivityInstanceSearchDescriptor.NAME, ACTIVITY_CONTAINER)
 			and()
 			differentFrom(ArchivedActivityInstanceSearchDescriptor.NAME, CREATE_ACTIVITY)
 			done()
-		}).getResult().collect{
-			if(!loopTasks.contains(it.name)) {
-				result << [id:it.name,name:it.displayName,state:[name:it.state,id:idOfState(it.state)],description:it.description]
-			}
-		}
-		
-		//Retrieve N/A activities
-		designProcessDefinition.getFlowElementContainer().getActivities().findAll{
-			it instanceof HumanTaskDefinition && !result.id.contains(it.name) && !ACTIVITY_CONTAINER.equals(it.name) && !CREATE_ACTIVITY.equals(it.name)
-		}.collect{
-			result << [id:it.name,name:it.name,state:[name:"N/A",id:idOfState("N/A")],description:it.description]
-		}
-		
-        return buildResponse(responseBuilder, HttpServletResponse.SC_OK, new JsonBuilder(result.sort {a1,a2 ->
-			 a1.state.id <=> a2.state.id
-		}).toString())
-    }
-	
-	
+		}).getResult()
+		.findAll{
+			!loopTasks.contains(it.name) }
+		.collect{ ArchivedHumanTaskInstance task ->
+			[
+				id:task.sourceObjectId,
+				name:task.displayName ?: task.name,
+				description:task.description,
+				state:task.state.capitalize()
+			]
+		})
+
+		buildResponse(responseBuilder, HttpServletResponse.SC_OK, new JsonBuilder(result).toString())
+	}
+
+	def getMetadata(HumanTaskInstance task, ProcessAPI processAPI) {
+		def res = [:]
+		processAPI.getActivityTransientDataInstances(task.id, 0, Integer.MAX_VALUE)
+				.findAll{ it.name.startsWith(PREFIX) }
+				.collect{ res.put(it.name,it.value) }
+		res
+	}
+
+
 	def String forge(String processName,String processVersion,ActivityInstance instance,contextPath) {
 		if(instance instanceof UserTaskInstance) {
 			"$contextPath/portal/resource/taskInstance/$processName/$processVersion/$instance.name/content/?id=$instance.id&displayConfirmation=false"
@@ -96,7 +134,7 @@ class CaseActivity implements RestApiController,CaseActivityHelper {
 			"$contextPath/apps/cases/do?id=$instance.id"
 		}
 	}
-	
+
 	def String linkTarget(ActivityInstance instance) {
 		if(instance instanceof UserTaskInstance) {
 			"_self"
@@ -105,21 +143,21 @@ class CaseActivity implements RestApiController,CaseActivityHelper {
 		}
 	}
 
-    /**
-     * Build an HTTP response.
-     *
-     * @param  responseBuilder the Rest API response builder
-     * @param  httpStatus the status of the response
-     * @param  body the response body
-     * @return a RestAPIResponse
-     */
-    RestApiResponse buildResponse(RestApiResponseBuilder responseBuilder, int httpStatus, Serializable body) {
-        return responseBuilder.with {
-            withResponseStatus(httpStatus)
-            withResponse(body)
-            build()
-        }
-    }
+	/**
+	 * Build an HTTP response.
+	 *
+	 * @param  responseBuilder the Rest API response builder
+	 * @param  httpStatus the status of the response
+	 * @param  body the response body
+	 * @return a RestAPIResponse
+	 */
+	RestApiResponse buildResponse(RestApiResponseBuilder responseBuilder, int httpStatus, Serializable body) {
+		return responseBuilder.with {
+			withResponseStatus(httpStatus)
+			withResponse(body)
+			build()
+		}
+	}
 
 
 }
