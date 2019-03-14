7a0f9e59d82646f4fb4f037efeca73c94b9c23c4
Pablo Alonso <pablo.alonso@bonitasoft.com>
2019-03-14 13:54:58 +0100

    Fix on rest apis and refresh on case list

--- restAPIExtensions/caseActivityRestAPI/src/main/groovy/com/bonitasoft/rest/api/CaseActivity.groovy
+++ restAPIExtensions/caseActivityRestAPI/src/main/groovy/com/bonitasoft/rest/api/CaseActivity.groovy
@@ -144,7 +144,7 @@ class CaseActivity implements RestApiController,CaseActivityHelper {
 
 	def String forge(String processName,String processVersion,ActivityInstance instance,contextPath) {
 		if(instance instanceof UserTaskInstance) {
-			"$contextPath/portal/form/taskInstance/?id=$instance.id&displayConfirmation=false"
+			"$contextPath/portal/form/taskInstance/$instance.id?displayConfirmation=false"
 		}else if(instance instanceof ManualTaskInstance) {
 			"$contextPath/apps/cases/do?id=$instance.id"
 		}
