5d4d32a6f80d6d35be7349cde8c114493f36be49
Pablo Alonso <pablo.alonso@bonitasoft.com>
2019-03-25 18:04:22 +0100

    Latest fixes on Forms

--- restAPIExtensions/caseActivityRestAPI/src/main/groovy/com/bonitasoft/rest/api/CaseActivity.groovy
+++ restAPIExtensions/caseActivityRestAPI/src/main/groovy/com/bonitasoft/rest/api/CaseActivity.groovy
@@ -63,6 +63,7 @@ class CaseActivity implements RestApiController,CaseActivityHelper {
 				}.collect{ it.name }
 				
 		//Retrieve pending activities
+		/*		
 		SearchOptionsBuilder sob = new SearchOptionsBuilder(0, Integer.MAX_VALUE);
 		def result = processAPI.searchAssignedAndPendingHumanTasksFor(pDef.id, context.apiSession.userId, sob.done()).getResult()
 		.findAll{it.name != ACTIVITY_CONTAINER && it.name != CREATE_ACTIVITY && it.parentProcessInstanceId == Long.parseLong(caseId) }
@@ -77,7 +78,7 @@ class CaseActivity implements RestApiController,CaseActivityHelper {
 				metadata:getMetadata(task,processAPI)
 			]
 		}.sort{ t1,t2 -> valueOf(t1.metadata.$activityState) <=> valueOf(t2.metadata.$activityState) }
-		/*		
+		*/
 		def result = processAPI.getPendingHumanTaskInstances(context.apiSession.userId,0, Integer.MAX_VALUE, ActivityInstanceCriterion.EXPECTED_END_DATE_ASC)
 				.findAll{it.name != ACTIVITY_CONTAINER && it.name != CREATE_ACTIVITY && it.parentProcessInstanceId == Long.parseLong(caseId) }
 				.collect{ HumanTaskInstance task ->
@@ -91,7 +92,7 @@ class CaseActivity implements RestApiController,CaseActivityHelper {
 						metadata:getMetadata(task,processAPI)
 					]
 				}.sort{ t1,t2 -> valueOf(t1.metadata.$activityState) <=> valueOf(t2.metadata.$activityState) }
-*/
+
 		def containerInstance = processAPI.searchHumanTaskInstances(new SearchOptionsBuilder(0, 1)
 				.filter(HumanTaskInstanceSearchDescriptor.PROCESS_INSTANCE_ID, caseId)
 				.filter(HumanTaskInstanceSearchDescriptor.NAME, ACTIVITY_CONTAINER)
